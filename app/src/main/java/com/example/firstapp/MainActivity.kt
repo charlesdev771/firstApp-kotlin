3c58da0fd03faf580db306ffe342012afff14ba0
charlesdev0771 <charles077211@gmail.com>
2024-10-24 01:40:58 -0300

    First app

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.firstapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun generate(view: View)
+    {
+        val text_result = findViewById<TextView>(R.id.resultText);
+        val number_random = Random.nextInt(1, 101)
+        text_result.setText("Number: $number_random");
+    }
+
+}
